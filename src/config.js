58165970638f4e5afcd1b38541635ac9a20a69e4
Kimberly Cable <cableredi@comcast.net>
2020-01-23 17:34:59 -0700

    testing

--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:office@localhost/bedbugs',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api"
 };
--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -3,6 +3,8 @@ const logger = require('./logger')
 
 function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization')
+console.log('authToken', authToken)
+console.log('API_TOKEN', API_TOKEN)
 
   if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
     logger.error(`Unauthorized request to path: ${req.path}`)
